9a0fd12230af6ebfd0f532af06d06c22b23e654f
Charlie B <charlie@ubuntu>
2021-03-10 07:24:55 -0800

    remove redundant line

--- speedtest_script.sh
+++ speedtest_script.sh
@@ -5,11 +5,6 @@ then
     rm results.csv
 fi
 
-if [ -e results_cut.csv ]
-then
-    rm results_cut.csv
-fi
-
 for i in {1..3}; do 
 	echo "Running speed test: $i/3"
 	echo "Speed test: $i" >> results.csv
